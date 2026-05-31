355e3b29c0b834c54d2daed8a9826c15264e789e
Ben Ashurst <benashurst@MacBookPro.lan>
2026-05-31 14:03:16 +0100

    Refine premium app styling

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "jumpseat-calendar-v1";
+const CACHE_NAME = "jumpseat-calendar-v3";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js?v=home-screen-1",
+  "./styles.css?v=premium-2",
+  "./app.js?v=premium-2",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
